b00977dbbe6696b94a66f896c3c9d4dacdaa41fb
Alexander M Artamonov <art.alex.m@gmail.com>
2024-01-26 14:43:10 +0300

    Реализация хранилища учетных данных

--- domain-auth/src/main/java/io/ylab/monitoring/domain/auth/repository/UserLoginInputDbRepository.java
+++ domain-auth/src/main/java/io/ylab/monitoring/domain/auth/repository/UserLoginInputDbRepository.java
@@ -4,6 +4,15 @@ import io.ylab.monitoring.domain.auth.model.AuthUser;
 
 import java.util.Optional;
 
+/**
+ * Репозиторий пользовательских учетных записей в сценарии "Вход в систему"
+ */
 public interface UserLoginInputDbRepository {
+    /**
+     * Поиск учетной информации по логину пользователя
+     *
+     * @param username Логин
+     * @return Объект
+     */
     Optional<AuthUser> findByUsername(String username);
 }
--- domain-auth/src/main/java/io/ylab/monitoring/domain/auth/repository/UserRegistrationInputDbRepository.java
+++ domain-auth/src/main/java/io/ylab/monitoring/domain/auth/repository/UserRegistrationInputDbRepository.java
@@ -4,8 +4,23 @@ import io.ylab.monitoring.domain.auth.model.AuthUser;
 
 import java.util.Optional;
 
+/**
+ * Репозиторий пользовательских учетных записей в сценарии "Регистрация"
+ */
 public interface UserRegistrationInputDbRepository {
+    /**
+     * Сохраняет информацию о пользователе
+     *
+     * @param user Учетные данные
+     * @return Истина когда пользователь добавлен в базу
+     */
     boolean create(AuthUser user);
 
+    /**
+     * Поиск учетной информации по логину пользователя
+     *
+     * @param username Логин
+     * @return Объект
+     */
     Optional<AuthUser> findByUsername(String username);
 }

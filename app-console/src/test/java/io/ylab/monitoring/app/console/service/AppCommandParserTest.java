ca27359f3136a744775c2abfb62b87deaec47d1c
Alexander M Artamonov <art.alex.m@gmail.com>
2024-01-28 15:35:20 +0300

    Сборка проекта и запуск

---
+++ app-console/src/test/java/io/ylab/monitoring/app/console/service/AppCommandParserTest.java
@@ -0,0 +1,32 @@
+package io.ylab.monitoring.app.console.service;
+
+import io.ylab.monitoring.app.console.model.AppCommand;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class AppCommandParserTest {
+    public static Stream<Arguments> parse() {
+        return Stream.of(
+                Arguments.of("test 1 2 3", new AppCommand(List.of("test", "1", "2", "3"))),
+                Arguments.of("test  ", new AppCommand(List.of("test"))),
+                Arguments.of("", new AppCommand(List.of(""))),
+                Arguments.of("some-test <arg1>   <arg2>", new AppCommand(List.of("some-test", "<arg1>", "<arg2>")))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void parse(String text, AppCommand expected) {
+        AppCommandParser sut = new AppCommandParser();
+
+        AppCommand result = sut.parse(text);
+
+        assertThat(result.hashCode()).isEqualTo(expected.hashCode());
+    }
+}

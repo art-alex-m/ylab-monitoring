dcd7fed10a1ef88d3c672361cdb764c2cb8740ac
Alexander M Artamonov <art.alex.m@gmail.com>
2024-02-08 12:14:36 +0300

    Инициация сервлет приложения

---
+++ app-servlets/src/main/java/io/ylab/monitoring/app/servlets/MonitoringApplication.java
@@ -0,0 +1,9 @@
+package io.ylab.monitoring.app.servlets;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class MonitoringApplication extends Application {
+
+}

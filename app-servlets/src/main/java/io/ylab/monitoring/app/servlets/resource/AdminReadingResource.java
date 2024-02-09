f719b162dcba71209446bed13f1290cb0910f15e
Alexander M Artamonov <art.alex.m@gmail.com>
2024-02-09 19:58:08 +0300

    Запросы ресурсов администратора

---
+++ app-servlets/src/main/java/io/ylab/monitoring/app/servlets/resource/AdminReadingResource.java
@@ -0,0 +1,75 @@
+package io.ylab.monitoring.app.servlets.resource;
+
+import io.ylab.monitoring.app.servlets.in.AppMonthReadingRequest;
+import io.ylab.monitoring.app.servlets.service.AppUserContext;
+import io.ylab.monitoring.core.in.CoreGetActualMeterReadingsInputRequest;
+import io.ylab.monitoring.core.in.CoreGetMonthMeterReadingsInputRequest;
+import io.ylab.monitoring.core.in.CoreViewMeterReadingsHistoryInputRequest;
+import io.ylab.monitoring.domain.core.boundary.GetActualMeterReadingsInput;
+import io.ylab.monitoring.domain.core.boundary.GetMonthMeterReadingsInput;
+import io.ylab.monitoring.domain.core.boundary.ViewMeterReadingsHistoryInput;
+import io.ylab.monitoring.domain.core.in.GetActualMeterReadingsInputRequest;
+import io.ylab.monitoring.domain.core.in.GetMonthMeterReadingsInputRequest;
+import io.ylab.monitoring.domain.core.in.ViewMeterReadingsHistoryInputRequest;
+import io.ylab.monitoring.domain.core.model.MeterReading;
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import jakarta.validation.Valid;
+import jakarta.ws.rs.BeanParam;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+import java.util.List;
+
+@Path("/admin/reading")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@RolesAllowed({"ADMIN"})
+public class AdminReadingResource {
+
+    @Inject
+    private AppUserContext userContext;
+
+    @Inject
+    @Named("adminActualMeterReadingsInteractor")
+    private GetActualMeterReadingsInput actualReadingInteractor;
+
+    @Inject
+    @Named("adminMeterReadingsHistoryInteractor")
+    private ViewMeterReadingsHistoryInput historyInteractor;
+
+    @Inject
+    @Named("adminMonthMeterReadingsInteractor")
+    private GetMonthMeterReadingsInput monthInteractor;
+
+    @GET
+    @Path("/actual")
+    public List<MeterReading> actual() {
+        GetActualMeterReadingsInputRequest request = new CoreGetActualMeterReadingsInputRequest(
+                userContext.getCurrentUser());
+
+        return actualReadingInteractor.find(request).getMeterReadings();
+    }
+
+    @GET
+    @Path("/history")
+    public List<MeterReading> history() {
+        ViewMeterReadingsHistoryInputRequest request = new CoreViewMeterReadingsHistoryInputRequest(
+                userContext.getCurrentUser());
+
+        return historyInteractor.find(request).getMeterReadings();
+    }
+
+    @GET
+    @Path("/month")
+    public List<MeterReading> month(@Valid @BeanParam AppMonthReadingRequest request) {
+        GetMonthMeterReadingsInputRequest coreRequest = new CoreGetMonthMeterReadingsInputRequest(
+                userContext.getCurrentUser(), request.getPeriod());
+
+        return monthInteractor.find(coreRequest).getMeterReadings();
+    }
+}
---
+++ app-servlets/src/main/java/io/ylab/monitoring/app/servlets/resource/AuditLogResource.java
@@ -0,0 +1,35 @@
+package io.ylab.monitoring.app.servlets.resource;
+
+import io.ylab.monitoring.app.servlets.service.AppUserContext;
+import io.ylab.monitoring.audit.in.AuditViewAuditLogInputRequest;
+import io.ylab.monitoring.domain.audit.boundary.ViewAuditLogInput;
+import io.ylab.monitoring.domain.audit.in.ViewAuditLogInputRequest;
+import io.ylab.monitoring.domain.audit.model.AuditItem;
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+import java.util.List;
+
+@Path("/admin/audit-log")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@RolesAllowed({"ADMIN"})
+public class AuditLogResource {
+
+    @Inject
+    private ViewAuditLogInput auditLogInteractor;
+
+    @Inject
+    private AppUserContext userContext;
+
+    @GET
+    public List<AuditItem> auditLog() {
+        ViewAuditLogInputRequest request = new AuditViewAuditLogInputRequest(userContext.getCurrentUser());
+        return auditLogInteractor.view(request).getAuditLog();
+    }
+}
